3f31eeeff6c8a2c0d679af48071cfb449939bf03
Alexander <nikal@kth.se>
2017-01-31 16:39:31 +0100

    Making connection to config-repo secure

--- configservice/src/main/java/configapp/ConfigLauncher.java
+++ configservice/src/main/java/configapp/ConfigLauncher.java
@@ -1,11 +1,13 @@
-package configlauncher;
+package configapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigServer
+@PropertySource("classpath:conf_cred.properties")
 public class ConfigLauncher {
     public static void main(String[] args) {
         SpringApplication.run(ConfigLauncher.class, args);
--- configservice/src/test/java/configapp/ConfigLauncherTest.java
+++ configservice/src/test/java/configapp/ConfigLauncherTest.java
@@ -1,4 +1,4 @@
-package configlauncher;
+package configapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
