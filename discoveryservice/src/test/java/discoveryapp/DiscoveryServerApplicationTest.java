afaa8279c29d9f5a5bd26ae7febcae1b8cbaab80
Alexander <nikal@kth.se>
2017-01-31 17:40:56 +0100

    Created Eureka discovery service

---
+++ discoveryservice/src/test/java/discoveryapp/DiscoveryServerApplicationTest.java
@@ -0,0 +1,25 @@
+package discoveryapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DiscoveryServerApplicationTest {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void testApps(){
+        ResponseEntity<String> apps = this.testRestTemplate.getForEntity("/eureka/apps", String.class);
+        then(apps.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
