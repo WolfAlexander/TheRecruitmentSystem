867c08b9095f6a4ea6784fae4b5c8a63b3fe9d78
Adrian Gortzak <addegor95@gmail.com>
2017-02-08 09:49:25 +0100

    Added exceptions and structured jobapplicationservice. Also removed that a request for a none existing application throws an exception

--- jobapplicationservice/src/main/java/jobApplicationApp/exception/NotValidArgumentException.java
+++ jobapplicationservice/src/main/java/jobApplicationApp/exception/NotValidArgumentException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class NotValidIdException extends Exception{
-    public NotValidIdException(String message){
+public class NotValidArgumentException extends IllegalArgumentException{
+    public NotValidArgumentException(String message){
         super(message);
     }
 }
--- jobapplicationservice/src/main/java/jobApplicationApp/service/JobApplicationService.java
+++ jobapplicationservice/src/main/java/jobApplicationApp/service/JobApplicationService.java
@@ -1,17 +1,20 @@
 package jobApplicationApp.service;
 
 import jobApplicationApp.dao.ApplicationDao;
+import jobApplicationApp.dto.ApplicationForm;
 import jobApplicationApp.dto.ApplicationParamForm;
+import jobApplicationApp.dto.AvailabilityForm;
+import jobApplicationApp.dto.CompetenceForm;
 import jobApplicationApp.entity.ApplicationEntity;
-import jobApplicationApp.entity.CompetenceEntity;
 import jobApplicationApp.entity.CompetenceProfileEntity;
-import jobApplicationApp.exception.NotValidIdException;
+import jobApplicationApp.exception.NotValidArgumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import java.util.Collection;
+import java.util.IllegalFormatCodePointException;
 import java.util.Map;
 
 /**
@@ -21,7 +24,7 @@ import java.util.Map;
 public class JobApplicationService {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
-    private final int PAGESIZE = 100;
+
     @Autowired
     @Qualifier("mysql")
     private ApplicationDao applicationDao;
@@ -31,8 +34,8 @@ public class JobApplicationService {
      * @param id of application
      * @return application
      */
-    public ApplicationEntity getApplicationById(int id) throws NotValidIdException {
-        validId(id);
+    public ApplicationEntity getApplicationById(int id) throws NotValidArgumentException {
+        validateId(id);
         ApplicationEntity applicationEntity = applicationDao.getApplicationById(id);
         log.info("Application with id " + String.valueOf(id) + " was retrieved from db");
         return applicationEntity;
@@ -43,57 +46,38 @@ public class JobApplicationService {
      * @param id on application we want to change status on
      * @param newStatus on application
      */
-    public void changeStatusOnApplicationById(int id, String newStatus) throws NotValidIdException {
-        validId(id);
+    public void changeStatusOnApplicationById(int id, String newStatus) throws NotValidArgumentException {
+        validateId(id);
         applicationDao.changeApplicationStatus(id,newStatus);
         log.info("Changing status on application with id " + id +" to " + newStatus);
     }
 
-    public void registerJobApplication(ApplicationEntity application) {
+    public void registerJobApplication(ApplicationForm application) throws NotValidArgumentException {
         applicationDao.insertApplication(application);
-        log.info("A new application was created by userId [" + application.getPerson().getId() + "]");
+        log.info("A new application was created by userId [" + application.getPersonId() + "]");
     }
 
-    public Collection<ApplicationEntity> getApplicationPage(int id) throws NotValidIdException {
-        validId(id*PAGESIZE);
-        Collection<ApplicationEntity>  applications = applicationDao.getXApplicationsFrom(id*PAGESIZE,PAGESIZE);
-        log.info("applications page " + id + " was retrieved");
+    public Collection<ApplicationEntity> getApplicationsPage(int pageSize ,int pageNmr) throws NotValidArgumentException {
+        Collection<ApplicationEntity>  applications = applicationDao.getXApplicationsFrom(pageNmr*pageSize,pageSize);
+        log.info("applications page " + pageNmr + " with size " + pageSize + "was retrieved");
         return applications;
     }
 
-    public Collection<ApplicationEntity> getApplicationByParam(ApplicationParamForm param) {
+    public Collection<ApplicationEntity> getApplicationsByParam(ApplicationParamForm param) {
         Map<Integer, ApplicationEntity> allApplication = applicationDao.getAllApplication();
 
-        if(param.getAvailableFrom() != null){
-            allApplication.forEach((k,v)->{
-                if(!(param.getAvailableFrom().before(v.getAvailableForWork().getFromDate()) && (v.getAvailableForWork().getToDate() == null || v.getAvailableForWork().getToDate().after(param.getAvailableFrom())))){
-                    allApplication.remove(v.getId());
-                }
-            });
+        if(param.getAvailability().getFromDate() != null){
+         allApplication = removeApplicationsThatIsNotAvailableAtRequiredStartDate(allApplication,param.getAvailability());
         }
 
-        if(param.getAvailableTo() != null){
-            allApplication.forEach((k,v)->{
-                if(v.getAvailableForWork().getToDate().after(param.getAvailableTo()) && (v.getAvailableForWork().getFromDate().before(param.getAvailableTo()) || v.getAvailableForWork().getFromDate() == null)){
-                    allApplication.remove(v.getId());
-                }
-            });
+        if(param.getAvailability().getToDate() != null){
+         allApplication = removeApplicationsThatIsNotAvailableToRequiredEndDate(allApplication,param.getAvailability());
         }
 
         if(param.getCompetences() != null){
-            allApplication.forEach((k,v)->{
-                Collection<CompetenceEntity> competences = param.getCompetences();
-                for(CompetenceProfileEntity c : v.getCompetenceProfile()){
-                    if(competences.contains(c)){
-                        competences.remove(c);
-                    }
-                }
-                if(competences.size() > 0){
-                    allApplication.remove(v.getId());
-                }
-            });
-
+            allApplication = removeApplicationThatDoesntContainCompetence(allApplication,param.getCompetences());
         }
+
         if(param.getName() != null){
             //Todo what is this!?
         }
@@ -101,13 +85,44 @@ public class JobApplicationService {
         return allApplication.values();
     }
 
-    private void validId(int id ) throws NotValidIdException {
+    private Map<Integer, ApplicationEntity> removeApplicationsThatIsNotAvailableAtRequiredStartDate(Map<Integer, ApplicationEntity> allApplication, AvailabilityForm availability){
+        allApplication.forEach((k,v)->{
+            if(!(availability.getFromDate().before(v.getAvailableForWork().getFromDate()) && (v.getAvailableForWork().getToDate() == null || v.getAvailableForWork().getToDate().after(availability.getFromDate())))){
+                allApplication.remove(v.getId());
+            }
+        });
+        return allApplication;
+    }
+
+    private Map<Integer, ApplicationEntity> removeApplicationsThatIsNotAvailableToRequiredEndDate(Map<Integer, ApplicationEntity> allApplication, AvailabilityForm availability) {
+        allApplication.forEach((k,v)->{
+            if(v.getAvailableForWork().getToDate().after(availability.getToDate()) && (v.getAvailableForWork().getFromDate().before(availability.getToDate()) || v.getAvailableForWork().getFromDate() == null)){
+                allApplication.remove(v.getId());
+            }
+        });
+        return allApplication;
+    }
+
+    private Map<Integer, ApplicationEntity> removeApplicationThatDoesntContainCompetence(Map<Integer, ApplicationEntity> allApplication, Collection<CompetenceForm> requiredCompetences){
+        allApplication.forEach((k,v)->{
+            Collection<CompetenceForm> competences = requiredCompetences;
+            for(CompetenceProfileEntity c : v.getCompetenceProfile()){
+                if(competences.contains(c)){
+                    competences.remove(c);
+                }
+            }
+            if(competences.size() > 0){
+                allApplication.remove(v.getId());
+            }
+        });
+        return allApplication;
+    }
+
+
+    private void validateId(int id ) throws NotValidArgumentException {
         if(id < 0){
-         throw new NotValidIdException("Id " + id + " is too low");
-        }
-        else if(!applicationDao.applicationExists(id))
-        {
-         throw new NotValidIdException("Id " + id +" was does not exist");
+            log.error("Request with illegal id ->" + id);
+            throw new NotValidArgumentException("Id " + id + " is too low");
         }
     }
 }
