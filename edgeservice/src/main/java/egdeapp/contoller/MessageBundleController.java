641c85a250a50435d70f6ca358a89b19930a2774
Alexander <nikal@kth.se>
2017-02-09 23:31:52 +0100

    Adding missing comments, fixing upp bug wtih response message from hystrix, adding more translate messages,  adding more error handling in UI-model

--- edgeservice/src/main/java/egdeapp/contoller/MessageBundleController.java
+++ edgeservice/src/main/java/egdeapp/contoller/MessageBundleController.java
@@ -7,15 +7,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Locale;
 import java.util.Properties;
 
+/**
+ * Controller for provides content text depending on a requested language
+ */
 @RestController
 public class MessageBundleController {
     private final BundleMessageSource messageSource;
 
+    /**
+     * Injecting source of where translated content can be found
+     * @param messageSource - representation that accesses messages in properties
+     */
     @Autowired
     public MessageBundleController(BundleMessageSource messageSource) {
         this.messageSource = messageSource;
     }
 
+    /**
+     * Returns content for a given language
+     * @param lang - requested language
+     * @return content in properties form
+     */
     @GetMapping("/messageBundle")
     public Properties getPropetiesByLanguage(@RequestParam String lang){
         return messageSource.getAllPropertiesForGivenLanguage(new Locale(lang));
