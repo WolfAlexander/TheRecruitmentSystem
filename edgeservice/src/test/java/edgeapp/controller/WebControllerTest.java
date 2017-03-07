7d42fc02c2b78fc6793f609c719d099d7eecf3b2
Alexander <nikal@kth.se>
2017-03-07 13:10:42 +0100

    Edge test fix

--- edgeservice/src/test/java/edgeapp/controller/WebControllerTest.java
+++ edgeservice/src/test/java/edgeapp/controller/WebControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * 1. Request home page
  */
 @RunWith(SpringRunner.class)
+@ActiveProfiles("testing")
 @SpringBootTest(classes = EdgeServiceApplication.class)
 @AutoConfigureMockMvc
 public class WebControllerTest {
