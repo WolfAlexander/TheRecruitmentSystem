e7337c90663bac199f6690157af1b5fcb028401f
Alexander <nikal@kth.se>
2017-03-07 12:55:12 +0100

    Fixing discovery service tests

--- discoveryservice/src/test/java/discoveryapp/DiscoveryServerApplicationTest.java
+++ discoveryservice/src/test/java/discoveryapp/DiscoveryServerApplicationTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.BDDAssertions.then;
  * Testing if Eureka service is app and running correctly
  */
 @RunWith(SpringRunner.class)
+@ActiveProfiles("testing")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DiscoveryServerApplicationTest {
     @Autowired
