e243718e9c898b0a71b2a167d78450b999c2c230
Alexander <nikal@kth.se>
2017-03-09 11:51:11 +0100

    Adding testing private/public keys, removing lang i registration controller

--- shell_scripts/build-services-docker-unix.sh
+++ shell_scripts/build-services-docker-unix.sh
@@ -20,14 +20,14 @@ echo
 echo
 
 #Building edgeservice with maven docker plugin
-echo Building edgeservice
-cd ..
-cd edgeservice
-mvn package docker:build -Ddocker.host=$DOCKER_HOST -Ddocker.cert.path=$DOCKER_CERT_PATH
-
-echo
-echo
-echo
+#echo Building edgeservice
+#cd ..
+#cd edgeservice
+#mvn package docker:build -Ddocker.host=$DOCKER_HOST -Ddocker.cert.path=$DOCKER_CERT_PATH
+#
+#echo
+#echo
+#echo
 
 #Building authentication-service with maven docker plugin
 echo Building authentication service
@@ -40,7 +40,7 @@ echo
 echo
 
 #Building registration-service with maven docker plugin
-echo Building registration service
-cd ..
-cd registration-service
-mvn package docker:build -Ddocker.host=$DOCKER_HOST -Ddocker.cert.path=$DOCKER_CERT_PATH
+#echo Building registration service
+#cd ..
+#cd registration-service
+#mvn package docker:build -Ddocker.host=$DOCKER_HOST -Ddocker.cert.path=$DOCKER_CERT_PATH
